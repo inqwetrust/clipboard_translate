d5c7db4bfc60c6b95ccaea3d7c61e89fa6ffab8e
iamanothergithubuser <32283030+iamanothergithubuser@users.noreply.github.com>
2019-10-21 15:06:56 +0800

    vv2

---
+++ clipboard_translate.py
@@ -0,0 +1,21 @@
+import subprocess
+from subprocess import Popen, PIPE
+# import commands
+import clipboard
+import os
+from translate import Translator
+
+text = ''
+while True:
+
+    if text != clipboard.paste() and len(clipboard.paste()) > 0:
+        print(clipboard.paste())
+        text = clipboard.paste()
+        translator = Translator(to_lang="zh-TW")
+        translation = Translator.translate(translator, str(text))
+        print(translation)
+        text = str(translation).replace('&gt;', '>')
+        clipboard.copy(str(translation).replace('&gt;', '>'))
+
+
+
